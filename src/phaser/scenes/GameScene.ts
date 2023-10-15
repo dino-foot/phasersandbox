83b11be229c3bb869578aaaef2cdc8adc573bd38
Shohanur Rahaman <shohan4556@gmail.com>
2023-10-15 14:16:36 +0800

    update

--- src/phaser/scenes/GameScene.ts
+++ src/phaser/scenes/GameScene.ts
@@ -1,30 +1,23 @@
 import Phaser from 'phaser'
 import { EventsController } from '../controllers/eventsController';
+import { Card } from '../objects';
 
 export class Game extends Phaser.Scene {
-  // private cursors!: Phaser.Types.Input.Keyboard.CursorKeys
   constructor() {
     super('game');
   }
 
   init() {
-    EventsController.onLostFocus(this.handleLostFocus);
-    EventsController.onResume(this.handResumeFocus);
-  }
-
-  handleLostFocus() {
-    console.log('game-lost-focus');
-  }
-
-  handResumeFocus() {
-    console.log('game-resume-focus');
+    // EventsController.onResume(this.handResumeFocus);
   }
 
   create() {
     this.add.image(this.cameras.main.centerX, this.cameras.main.centerY, 'bg').setOrigin(0.5);
-    this.add.image(100, 200, '1b');
-    this.add.image(200, 200, '1c');
-    this.add.image(300, 200, '1d');
+    // this.add.image(100, 200, '1b');
+    // this.add.image(200, 200, '1c');
+    // this.add.image(300, 200, '1d');
+
+    const card = new Card(this, 100, 300, '1d');
   }
 
   update() {}
