873e4111546d02748569646c5d867ee6c50f6a66
Shohanur Rahaman <shohan4556@gmail.com>
2023-10-21 13:46:44 +0800

    wip fx

--- src/phaser/scenes/GameScene.ts
+++ src/phaser/scenes/GameScene.ts
@@ -1,6 +1,5 @@
 import Phaser from 'phaser';
 import _ from 'lodash';
-import { EventsController } from '../controllers/eventsController';
 import { Card } from '../objects';
 import { PhaserHelpers, distributeCards } from '../helpers';
 import { scopaDeck } from '../constants';
@@ -11,9 +10,10 @@ export class Game extends Phaser.Scene {
   centerX: number;
   centerY: number;
   resetButton: Phaser.GameObjects.Text;
-  CardSuffle1_A: Phaser.GameObjects.Text;
-  CardSuffle1_B: Phaser.GameObjects.Text;
-  CardSuffle1_C: Phaser.GameObjects.Text;
+  shuffleABtn: Phaser.GameObjects.Text;
+  glowFXBtn: Phaser.GameObjects.Text;
+  shineFXBtn: Phaser.GameObjects.Text;
+  scopaFXBtn: Phaser.GameObjects.Text;
   player: Phaser.GameObjects.Image;
   constructor() {
     super('game');
@@ -39,6 +39,21 @@ export class Game extends Phaser.Scene {
     //? create deck
     this.resetDeck();
 
+    // todo
+    // - reset button
+    //? anim/fx
+    // card distribute tween
+    // variation A
+    // variation B
+    // card selection effect
+    // card place/show tween
+    // card match tween (placed 1 card, matched with 2 card)
+    // card shine fx
+    // card glow fx
+    // card match fx
+    // scopa win fx
+    // settabello win fx
+
     //? 4 card distribute
     const playerCards: Card[] = _.sampleSize(this.deck, 4);
     this.updateDeck(playerCards);
@@ -47,11 +62,27 @@ export class Game extends Phaser.Scene {
 
   createUI() {
     this.resetButton = PhaserHelpers.addText(GameSettings.RESET, this);
+    this.shineFXBtn = PhaserHelpers.addText(GameSettings.SHINE, this);
+    this.glowFXBtn = PhaserHelpers.addText(GameSettings.GLOW, this);
+    this.shuffleABtn = PhaserHelpers.addText(GameSettings.SHUFFLE_A, this);
+    this.scopaFXBtn = PhaserHelpers.addText(GameSettings.SCOPA_FX, this);
+
+    this.resetButton.on(
+      'pointerdown',
+      () => {
+        this.handleUIEvents('RESET');
+      },
+      this
+    );
   }
 
   updateDeck(subset: Card[]) {
     this.deck = _.differenceWith(this.deck, subset, (deckCard, subsetCard) => deckCard.cardValue === subsetCard.cardValue);
     // console.log('Updated Deck:', this.deck.length);
+  }
+
+  handleUIEvents(type: string) {
+    console.log(type);
   }
 
   resetDeck() {
--- src/phaser/scenes/GameSettings.ts
+++ src/phaser/scenes/GameSettings.ts
@@ -2,11 +2,32 @@ import { TextObject } from '../types';
 
 interface GameSettingsInterface {
   RESET: TextObject;
+  GLOW: TextObject;
+  SHINE: TextObject;
+  SCOPA_FX: TextObject;
+  SHUFFLE_A: TextObject;
 }
 
 export const GameSettings: GameSettingsInterface = {
   RESET: {
     text: 'RESET',
+    x: 100,
+    y: 50,
+    origin: {
+      x: 0.5,
+      y: 0.5,
+    },
+    style: {
+      fontSize: 16,
+      color: '#ffffff',
+      backgroundColor: '#ff00ff',
+    },
+    depth: 2,
+    isInterActive: true,
+  },
+
+  GLOW: {
+    text: 'GLOW CARD',
     x: 200,
     y: 50,
     origin: {
@@ -14,8 +35,59 @@ export const GameSettings: GameSettingsInterface = {
       y: 0.5,
     },
     style: {
-      fontSize: 24,
+      fontSize: 16,
       color: '#ffffff',
+      backgroundColor: '#ff00ff',
+    },
+    depth: 2,
+    isInterActive: true,
+  },
+
+  SHINE: {
+    text: 'SHINE CARD',
+    x: 300,
+    y: 50,
+    origin: {
+      x: 0.5,
+      y: 0.5,
+    },
+    style: {
+      fontSize: 16,
+      color: '#ffffff',
+      backgroundColor: '#ff00ff',
+    },
+    depth: 2,
+    isInterActive: true,
+  },
+
+  SCOPA_FX: {
+    text: 'SCOPA_FX',
+    x: 400,
+    y: 50,
+    origin: {
+      x: 0.5,
+      y: 0.5,
+    },
+    style: {
+      fontSize: 16,
+      color: '#ffffff',
+      backgroundColor: '#ff00ff',
+    },
+    depth: 2,
+    isInterActive: true,
+  },
+  SHUFFLE_A: {
+    text: 'SHUFFLE_A',
+    x: 500,
+    y: 50,
+    origin: {
+      x: 0.5,
+      y: 0.5,
+    },
+    style: {
+      fontSize: 16,
+      color: '#ffffff',
+      backgroundColor: '#ff00ff',
     },
     depth: 2,
     isInterActive: true,
