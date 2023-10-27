1591cde58da9e190ebfe1a2bd2466df8575654ae
Shohanur Rahaman <shohan4556@gmail.com>
2023-10-27 22:13:13 +0800

    lobby creation wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import './PhaserGame';
+import config from './PhaserGame';
+// import './PhaserGame';
 
 function App() {
-  return (<div id='phaser-container' className='App'></div>);
+  const [gameStarted, setGameStarted] = useState(false);
+
+  const startGame = () => {
+    setGameStarted(true);
+  };
+
+  useEffect(() => {
+    if (gameStarted) {
+      const game = new Phaser.Game(config);
+      return () => {
+        game.destroy(true);
+      };
+    }
+  }, [gameStarted]);
+
+  return (<div id='App' className='App'>
+    <div id='phaser-container'></div>
+    {!gameStarted && <button onClick={startGame}> Start Game </button>}
+    {/* <p>lobby page</p> */}
+  </div>);
 }
 
 export default App;
--- src/PhaserGame.ts
+++ src/PhaserGame.ts
@@ -17,5 +17,5 @@ const config: Phaser.Types.Core.GameConfig = {
   scene: [PreloadScene, ScopaScene, OkeyScene],
 };
 
-// eslint-disable-next-line import/no-anonymous-default-export
-export default new Phaser.Game(config)
+// export default new Phaser.Game(config);
+export default config;
