d91eb9e84e338fcc5b15afcdc3228fd10aa84172
Shohanur Rahaman <shohan4556@gmail.com>
2023-11-04 00:32:26 +0800

    refactor :+1:

--- src/phaser/helpers/OkeyHelpers.ts
+++ src/phaser/helpers/OkeyHelpers.ts
@@ -1,13 +1,36 @@
 export function parseZoneName(data: string) {
-  const label = data.split("_")[0];
-  const value = parseInt(data.split("_")[1]); //
-  return { label: label, value: value };
+    const label = data.split("_")[0];
+    const value = parseInt(data.split("_")[1]); //
+    return { label: label, value: value };
 }
 
 export function determineZoneType(zoneName: string) {
-  if (zoneName.includes("zone_top")) {
-    return "top";
-  } else {
-    return "bottom";
-  }
+    if (zoneName.includes("zone_top")) {
+        return "top";
+    } else {
+        return "bottom";
+    }
+}
+
+export function getShiftableZones(zones:Phaser.GameObjects.Zone[], targetZone:Phaser.GameObjects.Zone) {
+    const targetIndex = zones.findIndex((zone) => zone.name === targetZone.name);
+    const shiftZones = [];
+
+    // Check right adjacent zones
+    for (let i = targetIndex + 1; i < zones.length; i++) {
+        if (!zones[i].getData("isOccupied")) {
+            return shiftZones; // Return the list of zones from target to first unoccupied zone
+        }
+        shiftZones.push(zones[i]);
+    }
+
+    // Check left adjacent zones
+    for (let i = targetIndex - 1; i >= 0; i--) {
+        if (!zones[i].getData("isOccupied")) {
+            return shiftZones.reverse(); // Return the list of zones from target to first unoccupied zone
+        }
+        shiftZones.push(zones[i]);
+    }
+
+    return shiftZones.reverse(); // Return the entire list if all zones are occupied
 }
