137b26de4a9bd635608ad9ecafeb7a1f078fefac
Shohanur Rahaman <shohan4556@gmail.com>
2023-11-04 22:02:11 +0800

    Update OkeyHelpers.ts

--- src/phaser/helpers/OkeyHelpers.ts
+++ src/phaser/helpers/OkeyHelpers.ts
@@ -1,3 +1,5 @@
+import { tweenPosition } from "./TweensHelpers";
+
 export function parseZoneName(data: string) {
     const label = data.split("_")[0];
     const value = parseInt(data.split("_")[1]); //
@@ -14,26 +16,32 @@ export function determineZoneType(zoneName: string) {
 
 export function getAdjacentOccupiedZones(zones:Phaser.GameObjects.Zone[], targetZone:Phaser.GameObjects.Zone): any {
     const targetIndex = zones.findIndex((zone) => zone.name === targetZone.name);
-    const occupiedZones = [];
+    let occupiedZones = [];
     let direction = null;
 
-    // Check right adjacent zones
-    for (let i = targetIndex ; i < zones.length; i++) {
-        if (!zones[i].getData("isOccupied")) {
-            direction = 'right';
-            return {occupiedZones: occupiedZones.reverse(), direction}; // Return the list of zones from target to first unoccupied zone
+    if (targetIndex !== -1) {
+        // Check right adjacent zones if the target index is valid
+        for (let i = targetIndex; i < zones.length; i++) {
+            if (!zones[i].getData("isOccupied")) {
+                direction = 'right';
+                return { occupiedZones, direction };
+            }
+            occupiedZones.push(i);
         }
-        occupiedZones.push(zones[i]);
+
+        // Check left adjacent zones if no unoccupied zone is found on the right
+        if (direction !== 'right') {
+            occupiedZones = [];
+            for (let i = targetIndex; i >= 0; i--) {
+                if (!zones[i].getData("isOccupied")) {
+                    direction = 'left';
+                    return { occupiedZones, direction }; // Stop checking left adjacent zones if an unoccupied zone is found
+                }
+                occupiedZones.push(i);
+            }
+        }
     }
 
-    // Check left adjacent zones
-    for (let i = targetIndex; i >= 0; i--) {
-        if (!zones[i].getData("isOccupied")) {
-            direction = 'left';
-            return {occupiedZones: occupiedZones.reverse(), direction}; // Return the list of zones from target to first unoccupied zone
-        }
-        occupiedZones.push(zones[i]);
-    }
-
-    return {occupiedZones: occupiedZones.reverse(), direction}; // Return the entire list if all zones are occupied
+    return { occupiedZones, direction }; // Return the entire list if all zones are occupied
 }
+
