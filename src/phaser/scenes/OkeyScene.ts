4f61d5bd7b1337af09d0ee77b97851c9c5903920
Shohanur Rahaman <shohan4556@gmail.com>
2023-11-09 01:34:41 +0800

    cleanup codes

--- src/phaser/scenes/OkeyScene.ts
+++ src/phaser/scenes/OkeyScene.ts
@@ -1,4 +1,4 @@
-import Phaser from 'phaser';
+import Phaser, { Input } from 'phaser';
 import { GameObjects, Scene } from 'phaser';
 import _ from 'lodash';
 import {
@@ -56,6 +56,35 @@ export class OkeyScene extends Phaser.Scene {
   //? implement desktop drag and drop grouped stones.  The "group move" button appears when you hover on a group. (wip)
 
   create() {
+
+    //? ------- add drag events ------
+    this.input.on('drop', (pointer, gameObject, dropZone) => {
+      // console.log('drop ', dropZone.name);
+      this.handleDragEvents('drop', pointer, gameObject, null, null, dropZone);
+    });
+
+    this.input.on('dragend', (pointer, gameObject, dropped) => {
+      this.handleDragEvents('dragend', pointer, gameObject, null, null, null, dropped);
+    });
+
+    this.input.on("dragenter", (pointer, gameObject, dropZone) => {
+      this.targetDropZone = dropZone;
+      if (this.lastDropZone === null) {
+        this.lastDropZone = this.targetDropZone;
+      }
+      if (this.lastDropZone && this.lastDropZone !== this.targetDropZone) {
+        addHighLight(this, dropZone);
+      }
+      // console.log(`dragenter >> target: ${gameObject.name} | last: ${dropZone?.name}`);
+    });
+
+
+    this.input.on("dragleave", (pointer, gameObject, dropZone) => {
+      this.targetDropZone = null;
+      clearHighLight(dropZone);
+      // console.log('dragleave ', dropZone.name);
+    });
+
     this.dealStone = PhaserHelpers.addText(TextSettings.DEAL_CARDS, this);
     this.resetButton = PhaserHelpers.addText(TextSettings.RESET, this);
 
@@ -103,154 +132,23 @@ export class OkeyScene extends Phaser.Scene {
 
       //? ----- object events ------
       card.on("drag", (pointer, dragX, dragY) => {
-        this.handleDragEvents("drag", pointer, card, dragX, dragY, null);
+        this.handleDragEvents("drag", pointer, card, dragX, dragY);
 
-      });
-
-      card.on("dragend", (pointer, dragX, dragY, dropped) => {
-        this.handleDragEvents("dragend", pointer, card, dragX, dragY, dropped);
       });
 
       card.on("dragstart", (pointer, dragX, dragY) => {
-        this.handleDragEvents("dragstart", pointer, card, dragX, dragY, null);
-      });
-
-      //? ------- drag events ------
-      this.input.on("dragenter", (pointer, gameObject, dropZone) => {
-        this.targetDropZone = dropZone;
-        if (this.lastDropZone === null) {
-          this.lastDropZone = this.targetDropZone;
-        }
-
-        if (this.lastDropZone && this.lastDropZone !== this.targetDropZone) {
-          addHighLight(this, dropZone);
-        }
-        // console.log(`dragenter >> target: ${gameObject.name} | last: ${dropZone?.name}`);
-      });
-
-      this.input.on("dragleave", (pointer, gameObject, dropZone) => {
-        this.targetDropZone = null;
-        clearHighLight(dropZone);
-        // console.log('dragleave ', dropZone.name);
-      });
-
-      card.on("pointerup", (pointer, localX, localY) => {
-        // console.log('pointerup ', this.lastDropZone?.name);
-        this.handleDropEvent(pointer, card, this.targetDropZone);
+        this.handleDragEvents("dragstart", pointer, card, dragX, dragY);
       });
     }
   }
 
-  handleDropEvent(pointer, gameObject, dropZone) {
-    // console.log(`pointerup >> target: ${this.targetDropZone?.name} | last: ${this.lastDropZone?.name}`);
 
-    if (!dropZone) {
-      //? invalid zone
-      this.handleInvalidZone(gameObject);
-      return;
-    }
-
-    //? handle un-occupied zone
-    if (dropZone.getData("isOccupied") === false) {
-      // empty zone
-      this.assignToZone(gameObject, dropZone);
-
-      const zoneList = determineZoneType(dropZone.name) === "top" ? this.zoneTop : this.zoneBottom;
-      const adjacentCards = getAdjacentCards(zoneList, dropZone);
-      const groupedCards = getGroupedCards(adjacentCards);
-
-      // todo 
-      // drag n drop 
-      // check if there  any empty zone.length = graup length 
-
-      for (const key in groupedCards) {
-        if (groupedCards[key].length > 0) {
-
-          const startCard = groupedCards[key][0];
-
-          groupedCards[key].container = createContainer(this, this.cardWidth * groupedCards[key].length, this.cardHeight, false)
-          groupedCards[key].container.setPosition(startCard.x + this.cardWidth, startCard.y);
-          // groupedCards[key].container.setDepth(11);
-          // console.log('width ', this.cardWidth * groupedCards[key].length);
-
-          groupedCards[key].dragIcon = this.add.image(0, 0, 'plus-icon').setName('plus-icon').setScale(1.1).setDepth(11);
-          Phaser.Display.Align.In.TopRight(groupedCards[key].dragIcon, groupedCards[key].container, 20, 25);
-          // groupedCards[key].container.add(dragIcon);
-
-          // attach event 
-          groupedCards[key].container.setInteractive();
-          this.input.setDraggable(groupedCards[key].container);
-          
-          groupedCards[key].container.on('pointerover', (pointer, gameObject) => { this.handleGroupDranNDrop('pointerover', groupedCards[key]) }, this);
-          groupedCards[key].container.on('pointerout', (pointer, gameObject) => { this.handleGroupDranNDrop('pointerout', groupedCards[key]) }, this);
-          groupedCards[key].container.on("drag", (pointer, dragX, dragY) => {
-            this.handleGroupDranNDrop('drag', groupedCards[key], dragX, dragY)
-          });
-
-          this.input.on('drop', (pointer, container, dropZone) => {
-            // console.log('drop ', dropZone.name);
-            this.handleGroupDranNDrop('drag', container, null, null, dropZone);
-          });
-
-          // highlight 
-          const containerBounds = groupedCards[key].container.input.hitArea;
-          const rect = createRectangle(this, { x: groupedCards[key].container.x, y: groupedCards[key].container.y }, containerBounds.width, containerBounds.height);
-          groupedCards[key].rect = rect;
-
-          addGlow(this, [rect], 0x33FF93, 350, 2);
-          _.forEach(groupedCards[key], (card, index) => {
-            card?.setPosition((index - 1) * this.cardWidth, 0);
-            card?.setAlpha(0.8);
-            card.input.enabled = false;
-            groupedCards[key].container.add(card);
-          });
-        }
-      }
-
-      console.log('grouped cards ', groupedCards);
-      // console.log({ occupiedZones, direction }, getCardsNamesFromZone(occupiedZones));
-
-      if (this.lastDropZone && this.lastDropZone !== this.targetDropZone) {
-        this.resetZone();
-      }
-    } else {
-      //? handle occupied zone
-      const zoneList = determineZoneType(dropZone.name) === "top" ? this.zoneTop : this.zoneBottom;
-      const { occupiedZones, direction } = getAdjacentOccupiedZones(zoneList, dropZone);
-
-      // all zones are filled 
-      if (direction === null) {
-        this.handleInvalidZone(gameObject);
-        return;
-      }
-      // console.log({ occupiedZones, direction });
-      // console.log(`target ${this.targetDropZone.getData('data')?.name} last ${this.lastDropZone.getData('data')?.name} card ${gameObject.name}`);
-
-      // if (occupiedZones.length > 1 && this.targetDropZone !== this.lastDropZone) {
-      //   const targetIndex = zoneList.findIndex((zone) => zone.name === dropZone.name);
-      //   if (direction === 'right') {
-      //     shiftRightDirection(this, zoneList, targetIndex, occupiedZones, dropZone, gameObject);
-      //   } else if (direction === 'left') {
-      //     shiftLeftDirection(this, zoneList, targetIndex, occupiedZones, dropZone, gameObject);
-      //   }
-      // }
-      // else {
-      //   this.handleInvalidZone(gameObject);
-      // }
-
-      // testing
-      this.handleInvalidZone(gameObject);
-    }
-  }
-
-  handleGroupDranNDrop(event: 'pointerover' | 'pointerout' | 'drag' | 'drop', group, dragX?, dragY?, dropZone?) {
+  handleGroupDranNDrop(event: 'pointerover' | 'pointerout' | 'drag' | 'drop', group: any | GameObjects.Container, dragX?, dragY?, dropZone?) {
     switch (event) {
       case "pointerover":
-        // console.log('over');
         group.dragIcon?.setVisible(true);
         _.forEach(group.container.getAll(), (child, index) => {
           child.setAlpha(0.6);
-          // console.log('over', card.name);
         });
         break;
       case "pointerout":
@@ -266,12 +164,26 @@ export class OkeyScene extends Phaser.Scene {
         group.rect.setPosition(dragX, dragY);
         break;
       case "drop":
+        // high light 
+        const zoneList = this.getTargetZoneList(dropZone);
+        const index = this.getZoneIndex(dropZone);
+        const size = Math.round(group.getAll().length / 2);
 
+        const startIndex = Math.max(index - size, 0);
+        const rightSlice = _.slice(zoneList, index, index + size);
+        const leftSlice = _.slice(zoneList, startIndex, index);
+
+        const listToSearch = leftSlice.concat(rightSlice);
+        console.log('list to search ', listToSearch, zoneList[index].name, size);
+
+        group.setPosition(dropZone.x, dropZone.y);
         break;
     }
   }
 
-  handleDragEvents(event: "drag" | "dragend" | "dragstart", pointer, gameObject, dragX, dragY, dropped) {
+  handleDragEvents(event: "drag" | "dragstart" | "drop" | "dragend", pointer: Input.Pointer, gameObject, dragX?: number, dragY?: number,
+    dropZone?: GameObjects.Zone, dropped?: boolean) {
+
     switch (event) {
       case "drag":
         // console.log('drag ', gameObject);
@@ -282,6 +194,9 @@ export class OkeyScene extends Phaser.Scene {
         gameObject.angle = 0;
         gameObject.setScale(1);
         gameObject.depth -= 1;
+        if (!dropped) {
+          this.handleInvalidZone(gameObject);
+        }
         break;
       case "dragstart":
         gameObject.setScale(1.15);
@@ -289,8 +204,16 @@ export class OkeyScene extends Phaser.Scene {
         gameObject.angle = 5;
         // console.log(gameObject.depth);
         break;
-      default:
-        // Handle other events if necessary
+      case "drop":
+        //? handle empty zone 
+        if (dropZone.getData("isOccupied") === false) {
+          gameObject.setPosition(dropZone.x, dropZone.y);
+          this.assignToZone(gameObject, dropZone);
+        }
+        else {
+          // occupied zone
+          this.handleInvalidZone(gameObject);
+        }
         break;
     }
   } // end
@@ -306,6 +229,7 @@ export class OkeyScene extends Phaser.Scene {
     targetZone.setData("isOccupied", true);
     targetZone.setData("data", gameObject);
     clearHighLight(targetZone);
+    this.resetZone();
   }
 
   resetZone() {
@@ -316,5 +240,16 @@ export class OkeyScene extends Phaser.Scene {
     this.lastDropZone?.setData("data", null);
     this.lastDropZone = null;
     this.targetDropZone = null;
+  }
+
+  getTargetZoneList(dropZone) {
+    const zoneList = determineZoneType(dropZone.name) === "top" ? this.zoneTop : this.zoneBottom;
+    return zoneList;
+  }
+
+  getZoneIndex(dropZone) {
+    const zoneList = this.getTargetZoneList(dropZone);
+    const index = zoneList.findIndex((zone) => zone.name === dropZone.name);
+    return index;
   }
 } // end class
